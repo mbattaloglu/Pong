4c7574b27981a6c72f9d1e3bb754016aeb0609f0
mbattaloglu <mmbattaloglu@gmail.com>
2023-04-12 19:23:56 +0300

    AI Paddle Movement

--- Assets/Scripts/ComputerPaddle.cs
+++ Assets/Scripts/ComputerPaddle.cs
@@ -2,5 +2,31 @@ using UnityEngine;
 
 public class ComputerPaddle : Paddle
 {
+    public Rigidbody2D ballRb;
 
+    private void FixedUpdate()
+    {
+        if (this.ballRb.velocity.x > 0)
+        {
+            if (this.ballRb.position.y > this.transform.position.y)
+            {
+                this.rb.AddForce(Vector2.up * this.speed);
+            }
+            else if (this.ballRb.position.y < this.transform.position.y)
+            {
+                this.rb.AddForce(Vector2.down * this.speed);
+            }
+        }
+        else
+        {
+            if(this.transform.position.y > 0f)
+            {
+                rb.AddForce(Vector2.down * this.speed);
+            }
+            else if (this.transform.position.y < 0f)
+            {
+                rb.AddForce(Vector2.up * this.speed);
+            }
+        }
+    }
 }
